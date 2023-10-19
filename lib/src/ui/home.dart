5ce147174b90222ffe4493f011e855c2cddacb43
hieuruoi7797 <trantrunghieuse@gmail.com>
2023-10-20 00:20:54 +0700

    update home ui code

---
+++ lib/src/ui/home.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            opacity: 0.25,
+            fit: BoxFit.cover,
+            image: AssetImage("assets/png_images/bg_home.png")
+          )
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.2),
+                  decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 20.0,
+                          offset: Offset(
+                            0,6
+                          )
+                        )
+                      ],
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20)
+                  ),
+                  child: Column(
+                    children: [
+                      Container(
+                        margin:EdgeInsets.only(top: 66),
+                        child: Text("Hoang Mai",
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.height * 0.12,
+                  height: MediaQuery.of(context).size.height * 0.12,
+                  margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.15),
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(100)
+                  ),
+                  child: Column(
+                    children: [
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:splat_record/main.dart';
+import 'package:splat_record/src/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
